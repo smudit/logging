27197ede8f12084bbe8c6cb8de43b0338c5882e2
smudit <mudit.sharma@btinternet.com>
2020-09-05 21:34:15 +0100

    removed reference to bookie config

--- sportech/helper/logging/__init__.py
+++ sportech/helper/logging/__init__.py
@@ -1,8 +1,6 @@
 __config__ = 'sportech-helper-logging.yml'
 
-from bookie import config as configuration
 
-config = configuration.data('sportech.helper.logging')
 
 # internal imports should go after configuration not to get recursion
 from sportech.helper.logging.logger import get_logger  # noqa: E402
